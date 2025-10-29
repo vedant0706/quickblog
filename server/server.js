659ea02e7b1dbc61d59daedf2f6f14835741fcc8
Vedant Jadhav <vedantjadhav173@gmail.com>
2025-10-29 19:32:59 +0530

    fix bugs

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ app.get('/', (req, res) => res.send("API is Working"))
 app.use('/api/admin', adminRouter)
 app.use('/api/blog', blogRouter)
 
-const PORT = process.nextTick.PORT || 3000;  //enc / nextTick
+const PORT = process.nextTick.PORT || 3000; //he use here a process.env.PORT || 3000;
 
 app.listen(PORT, ()=>{
     console.log('Server is running on port ' + PORT)
