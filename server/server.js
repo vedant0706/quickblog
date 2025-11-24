93ea71694be9fa2f33d3d7e2b58a544874ee8f8b
Vedant Jadhav <vedantjadhav173@gmail.com>
2025-11-24 15:58:26 +0530

    changes on env file

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ await connectDB()
 
 // Middlewares
 app.use(cors({
-    origin: ['http://localhost:5173'],
+    origin: ['http://localhost:5173', 'https://gemai-client.vercel.app'],
     credentials: true,
 }))
 app.use(express.json())
